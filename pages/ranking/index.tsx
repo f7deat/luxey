6ea81e7eff448eb6f28545da43455047676f0d62
f7deat <tan.dct9x@gmail.com>
2023-04-10 09:49:04 +0700

    fix anchor

--- pages/ranking/index.tsx
+++ pages/ranking/index.tsx
@@ -31,18 +31,15 @@ export default function Ranking() {
             </Head>
             <SimpleHeader />
             <main className="md:p-10 px-4">
-                <Row gutter={16}>
-                    <Col md={6} xs={4}>
-                        <div className="mb-10 md:text-4xl">
+                <div className="md:flex gap-4 mb-10">
+                    <div className="md:w-1/5">
+                        <div className="md:text-4xl">
                             <div className={playfairDisplay.className}>Hermes bag</div>
                             <div className={playfairDisplay.className}>ranking</div>
                         </div>
-                        <Anchor
-                            items={getBags()}
-                        />
-                    </Col>
-                    <Col md={12} xs={20}>
-                        <div className="flex justify-end mb-20">
+                    </div>
+                    <div className="md:w-3/5 justify-end flex">
+                        <div className="flex justify-end">
                             <div className="border-b border-gray-500 flex items-center py-2">
                                 <MagnifyingGlassIcon className="mr-4 text-gray-400" />
                                 <div className={inter.className}>
@@ -50,10 +47,20 @@ export default function Ranking() {
                                 </div>
                             </div>
                         </div>
+                    </div>
+                </div>
+                <div className="flex gap-4">
+                    <div className="w-1/5">
+                        <Anchor
+                            affix
+                            items={getBags()}
+                        />
+                    </div>
+                    <div className="w-3/5">
                         {
                             getBags().map(bag => (
-                                <div key={bag.key} id={bag.key}>
-                                    <div className="flex mb-10 md:gap-10 gap-4">
+                                <div key={bag.key} className="pt-10" id={bag.key}>
+                                    <div className="flex md:gap-10 gap-4">
                                         <div className="md:h-40 md:w-52 w-32 h-32 bg-gray-500">
 
                                         </div>
@@ -73,8 +80,8 @@ export default function Ranking() {
                                 </div>
                             ))
                         }
-                    </Col>
-                </Row>
+                    </div>
+                </div>
             </main>
         </>
     )
